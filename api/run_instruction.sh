17fde0e1c7bb4c316181b4997761505f472750e5
Bardiya <bardiya.basafa@gmail.com>
2026-06-10 08:26:36 +0330

    run ins fixed 2

---
+++ api/run_instruction.sh
@@ -0,0 +1,15 @@
+# the command should be run in the api directory
+
+
+
+## linux and mac
+# require dotnet 9
+# may take a while for first time
+dotnet run --project ./Bank.presentation/
+# then ctrl click on the link in output 'Now listening on: http://localhost:5283'
+
+## windows 
+# require dotnet 9
+# may take a while for first time
+dotnet run --project .\Bank.presentation\
+# then ctrl click on the link in output 'Now listening on: http://localhost:5283'
