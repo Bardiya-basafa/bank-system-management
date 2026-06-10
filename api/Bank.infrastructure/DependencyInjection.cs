250735129b05eef306f1e2f532432669d0a1e7a5
Bardiya <bardiya.basafa@gmail.com>
2026-06-10 07:33:00 +0330

    customer controller

---
+++ api/Bank.infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Bank.infrastructure;
+
+using domain.RepositoryContracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+public static class DependencyInjection {
+    
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<ICustomerRepository, ICustomerRepository>();
+
+        return services;
+    }
+
+}
